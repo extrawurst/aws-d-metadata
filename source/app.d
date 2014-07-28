4c82fefdbd9b0e4ad4db89917b57fc75ecb6814b
Stephan Dilly <dilly.stephan@gmail.com>
2014-07-29 00:12:07 +0200

    -implemented http GET call (untested) -test application

--- source/app.d
+++ source/app.d
@@ -1,6 +1,11 @@
 import std.stdio;
 
+import awsd.metadata;
+
 shared static this()
 {
 	writeln("Edit source/app.d to start your project.");
+
+	writeln("%s", GetAwsData("/latest/meta-data/"));
+	writeln("%s", GetAwsData("/latest/meta-data/instance-id"));
 }
--- source/awsd/metadata.d
+++ source/awsd/metadata.d
@@ -1,15 +1,29 @@
 ﻿module awsd.metadata;
 
+import vibe.d;
+
 struct AwsMetaData
 {
 	string request;
 	string[] values; 
 }
 
+private static const EC2_BASEURL_METADATA = "http://169.254.169.254";
+
 public static AwsMetaData GetAwsData(string _request)
 {
-	AwsMetaData res;
-	res.request = _request;
+	AwsMetaData result;
+	result.request = _request;
 
-	return res;
+	requestHTTP(EC2_BASEURL_METADATA ~ _request,
+		(scope req) {},
+		(scope res) 
+		{
+			import std.string:split;
+			auto responseStr = res.bodyReader.readAllUTF8();
+			result.values = split(responseStr,'\n');
+			//logInfo("Response: %s", res.bodyReader.readAllUTF8());
+		});
+
+	return result;
 }
